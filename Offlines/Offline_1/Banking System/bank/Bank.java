fa3a24c6bfa740a35c9285e84717c2b993f29d77
mrtaz77 <mrtazwar23@gmail.com>
2023-11-23 18:36:20 +0600

    cleaner Bank class

--- Offlines/Offline_1/Banking System/bank/Bank.java
+++ Offlines/Offline_1/Banking System/bank/Bank.java
@@ -2,12 +2,17 @@ package bank;
 
 public class Bank {
     private double internalFund;
+    private double loanInterestRate;
     private int year;
     
-    public Bank(double internalFund){
+    public Bank(double internalFund,double loanInterestRate) {
         this.internalFund = internalFund;
+        this.loanInterestRate = loanInterestRate;
         year = 0;
     }
+
+    public double getLoanInterestRate() { return loanInterestRate; }
+    public void setLoanInterestRate(double loanInterestRate) { this.loanInterestRate = loanInterestRate; }
 
     public int getYear() { return year; }
     public void setYear(int year) { this.year = year; }
